e9ba9a543a843af06898791fa9ecef53499f5229
Eitan Suez <esuez@thoughtworks.com>
2014-12-05 15:12:10 -0600

    added [probably redundant at the moment] team spec

---
+++ src/test/groovy/eitan/belote/TeamSpec.groovy
@@ -0,0 +1,27 @@
+package eitan.belote
+
+import spock.lang.Specification
+
+class TeamSpec extends Specification
+{
+  Team team
+
+  def setup()
+  {
+    Player p1 = new Player(name: "john")
+    Player p2 = new Player(name: "jane")
+    team = new Team(first: p1, second: p2)
+  }
+  def "team initial score should be 0"()
+  {
+    expect:
+    team.score == 0
+  }
+
+  def "team players assigned"()
+  {
+    expect:
+    team.first.name == 'john'
+    team.second.name == 'jane'
+  }
+}
