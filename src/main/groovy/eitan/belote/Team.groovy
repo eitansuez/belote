97b60dfabff46d1ea36bbcac4626109dda55b8c9
Eitan Suez <esuez@thoughtworks.com>
2014-12-17 22:31:18 -0600

    removed implied public modifiers from two methods (ignore)

--- src/main/groovy/eitan/belote/Team.groovy
+++ src/main/groovy/eitan/belote/Team.groovy
@@ -5,13 +5,13 @@ class Team
   Player first
   Player second
 
-  public void setFirst(Player p)
+  void setFirst(Player p)
   {
     this.first = p
     p.team = this
   }
 
-  public void setSecond(Player p)
+  void setSecond(Player p)
   {
     this.second = p
     p.team = this
