95296e0bb7662e80652ec834f930adc086c5cab5
Eitan Suez <esuez@thoughtworks.com>
2014-12-20 10:06:13 -0600

    minor improvements

--- ui/web/my-script.js
+++ ui/web/my-script.js
@@ -7,26 +7,30 @@ function onResize(event) {
 }
 
 $(function() {
-    var backGround = new Path.Rectangle(view.bounds);
-    backGround.fillColor = 'green';
 
     loadCards();
 
     var a = Math.min(view.bounds.width, view.bounds.height);
-    table = new Rectangle(new Point(0, 0), new Size(a, a));
+    table = new Path.Rectangle({
+        topLeft: new Point(0, 0),
+        size: new Size(a, a)
+    });
+    table.fillColor = {
+        gradient: {
+            stops: ['#8af28a', '#038406']
+        },
+        origin: table.bounds.topCenter,
+        destination: table.bounds.bottomCenter
+    };
+
     var c = a / (2 + handAspectRatio);
     var b = handAspectRatio * c;
 
-    var areas = setupAreas(c, b);
+    var groups = setupAreas(c, b);
 
     var scale = c / cards['7_clubs'].height;
     scaleCards(scale);
     cardSeparation = new Size(cards['7_clubs'].bounds.width / 2, 0);
-
-    var groups = [];
-    for (var i=0; i<4; i++) {
-        groups.push(new Group());
-    }
 
     placeCards([
         cards['7_clubs'],
@@ -34,7 +38,7 @@ $(function() {
         cards['ace_diamonds'],
         cards['jack_clubs'],
         cards['9_spades']
-    ], areas[0], groups, 0);
+    ], groups, 0);
 
     //chooseCard([cards['7_clubs'], cards['jack_clubs']]);
 
@@ -44,7 +48,7 @@ $(function() {
         cards['10_diamonds'],
         cards['10_spades'],
         cards['jack_hearts']
-    ], areas[0], groups, 1);
+    ], groups, 1);
 
     placeCards([
         cards['ace_hearts'],
@@ -52,7 +56,7 @@ $(function() {
         cards['9_clubs'],
         cards['queen_clubs'],
         cards['jack_spades']
-    ], areas[0], groups, 2);
+    ], groups, 2);
 
     placeCards([
         cards['ace_clubs'],
@@ -60,7 +64,7 @@ $(function() {
         cards['7_spades'],
         cards['jack_diamonds'],
         cards['king_spades']
-    ], areas[0], groups, 3);
+    ], groups, 3);
 
     turnUpCard(cards['queen_hearts']);
 
@@ -79,27 +83,34 @@ function chooseCard(validCards) {
 }
 
 function setupAreas(c, b) {
-    var paths = [];
+    var groups = [];
 
     var path = new Path.Rectangle(
         new Rectangle(
             new Point(c, c + b),
             new Size(b, c))
     );
-    //path.strokeColor = 'black';
-    paths.push(path);
 
-    for (var i=0; i<3; i++)
+    for (var i=0; i<4; i++)
     {
+        var group = new Group(path);
+        group.style = {
+            strokeColor: 'black',
+            dashArray: [4, 10],
+            strokeWidth: 4,
+            strokeCap: 'round'
+        };
+        groups.push(group);
         path = path.clone();
-        path.rotate(90, table.center);
-        paths.push(path);
     }
 
-    return paths;
+    return groups;
 }
 
 function loadCards() {
+    var cardsLayer = new Layer();
+    cardsLayer.name = 'cards';
+
     $("#images_section img").each(function() {
         var img = $(this);
         var id = img.attr("id");
@@ -108,6 +119,7 @@ function loadCards() {
         cards[id] = card;
     });
 }
+
 function scaleCards(scale) {
     for (var card in cards) {
         cards[card].scale(scale);
@@ -116,9 +128,9 @@ function scaleCards(scale) {
 
 ////
 
-function placeCards(hand, path, groups, index) {
+function placeCards(hand, groups, index) {
     var card = hand[0];
-    placeCard(card, path.bounds.leftCenter + [card.bounds.width/2, 0]);
+    placeCard(card, groups[0].bounds.leftCenter + [card.bounds.width/2, 0]);
     groups[index].addChild(card);
 
     for (var i=1; i<hand.length; i++) {
@@ -126,11 +138,11 @@ function placeCards(hand, path, groups, index) {
         groups[index].addChild(hand[i]);
     }
 
-    groups[index].rotate(-90*index, table.center);
+    groups[index].rotate(-90*index, table.bounds.center);
 }
 
 function turnUpCard(card) {
-    placeCard(card, table.center);
+    placeCard(card, table.bounds.center);
 }
 
 function placeCard(card, position) {
